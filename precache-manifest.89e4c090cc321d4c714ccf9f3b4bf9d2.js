741bd31d43815de951b2d30385dfbded2c5a5250
AshutoshMishra22 <ashutoshmishraofficial22@gmail.com>
2021-03-16 15:13:00 +0530

    Updates

---
+++ precache-manifest.89e4c090cc321d4c714ccf9f3b4bf9d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df5bdb64e0477b1882de952269aa4ae",
+    "url": "/TFJS/index.html"
+  },
+  {
+    "revision": "dd87b114c96152253487",
+    "url": "/TFJS/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "1953542793e0240a6633",
+    "url": "/TFJS/static/js/2.019788e9.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/TFJS/static/js/2.019788e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd87b114c96152253487",
+    "url": "/TFJS/static/js/main.1c306399.chunk.js"
+  },
+  {
+    "revision": "d60121eb235898c9beb9",
+    "url": "/TFJS/static/js/runtime-main.13f7dbfe.js"
+  }
+]);
